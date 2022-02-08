6b244fc7b64137ac4d6cc0a12a3ccabcd8410c7f
MohamedHamed12 <90472426+MohamedHamed12@users.noreply.github.com>
2022-02-08 07:04:19 +0200

    Create B-The Cake Is a Lie.py

---
+++ codes/py/B-The Cake Is a Lie.py
@@ -0,0 +1,13 @@
+for i in range(int(input())):                 #for i in range t
+    x,y,k=map(int,input().split())            #input vars
+    if x>=y :                                 #to get the longest length if it x or y no matter
+         x,y=x,y                              #the longest length will take 1
+    else :                                    #the anthor wil take the same as lenght
+          x,y=y,x
+
+    if (x-1)+(x)*(y-1)==k:
+        print("YES")
+    else:
+        print("NO")
+        
+        
