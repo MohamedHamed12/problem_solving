1478784a207ca47d36fec953cde87931dee09126
MohamedHamed12 <90472426+MohamedHamed12@users.noreply.github.com>
2022-08-18 08:35:02 +0200

    Delete codes directory

--- codes/py/Designer_Door_Mat.py
+++
@@ -1,8 +0,0 @@
-a=".|."
-heigt,width=map(int,input().split())
-
-for i in range(int(heigt/2)):
-    print((((i+1)*2-1)*a).center(width,"-"))
-print("WELCOME".center(width,"-"))
-for i in range(int(heigt/2)-1,-1,-1):
-    print((((i+1)*2-1)*a).center(width,"-"))
--- codes/py/reversed string.py
+++
@@ -1,8 +0,0 @@
-for i in range(int(input())):
-    n = int(input())
-    m = list(map(int, input().split()))
-    # print(sorted(m))
-    # print(sorted(m[:int(len(m)/2)])[::-1])
-    print("Yes") if(m[:int(len(m)/2)] == sorted(m[:int(len(m)/2)])[::-1]
-                    and m[int(len(m)/2):] == sorted(m[int(len(m)/2):])) else print("No")
-    # print(m[int(len(m)/2):])
